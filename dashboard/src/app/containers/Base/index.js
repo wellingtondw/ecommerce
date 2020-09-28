b5d0580b78c75add3b43f3ab07f068dc69f8bb7d
wellington <wellingtonslpbi@hotmail.com>
2020-09-28 09:11:17 -0300

    create dashboard base style

--- dashboard/src/app/containers/Base/index.js
+++ dashboard/src/app/containers/Base/index.js
@@ -1,15 +1,30 @@
 import React from "react";
 
+const TopBar = () => {
+  return (
+    <div className="flex horizontal full-width">
+      <div className="flex-1 flex flex-start">
+        <p>Ver Loja</p>
+      </div>
+      <div className="flex-1 flex flex-end">
+        <p>Sair</p>
+      </div>
+    </div>
+  );
+};
+
 function Dashboard({ children, ...props }) {
   return (
     <div className="flex horizontal">
-      <div className="flex vertical">
+      <div className="flex vertical full-height">
         <p>Menu</p>
       </div>
 
-      <div className="flex vertical">
-        <div className="flex horizontal">Barra topo</div>
-        <div className="flex">{children}</div>
+      <div className="flex vertical full-width">
+        <div className="flex horizontal">
+          <TopBar />
+        </div>
+        <div className="flex full-height">{children}</div>
       </div>
     </div>
   );
--- dashboard/src/app/index.js
+++ dashboard/src/app/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import "../css/index.css";
 
 import { HashRouter as Router, Route } from "react-router-dom";
 
